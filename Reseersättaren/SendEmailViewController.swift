d521dd07bd26f5767cffb8964a67c736912b19ba
Jonatan Tegen <jonatan@Jonatans-MacBook-Pro-2.local>
2018-09-10 12:55:08 +0200

    Initial commit

---
+++ Reseersättaren/SendEmailViewController.swift
@@ -0,0 +1,112 @@
+//
+//  SendEmailViewController.swift
+//  Reseersättaren
+//
+//  Created by Jonatan Tegen on 2018-09-06.
+//  Copyright © 2018 Jonatan Tegen. All rights reserved.
+//
+
+import UIKit
+import MessageUI
+
+class SendEmailViewController: UIViewController, MFMailComposeViewControllerDelegate {
+    @IBOutlet weak var EmailTextInput: UITextView!
+    
+    var name: String?
+    var socialSecurity: String?
+    var fromStation: String?
+    var toStation: String?
+    var expectedArrival: String?
+    var actualArrival: String?
+    var bank: String?
+    var clearingNumber: String?
+    var account: String?
+    var paymentType: String?
+    
+    var emailText: String?
+    
+    
+    @IBAction func SendEmail(_ sender: Any) {
+        let mailComposeViewController = configuredMailComposeViewController()
+        if MFMailComposeViewController.canSendMail() {
+            self.present(mailComposeViewController, animated: true, completion: nil)
+        } else {
+            self.showSendMailErrorAlert()
+        }
+    }
+  
+    
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let emailSubject: String! = "Reklamation av resa"
+        let emailRecipient: [String]! = ["info@skanetrafiken.se"]
+        let mc: MFMailComposeViewController = MFMailComposeViewController()
+        
+        mc.mailComposeDelegate = self
+        
+        mc.setSubject(emailSubject)
+        mc.setMessageBody(emailText!, isHTML: false)
+        mc.setToRecipients(emailRecipient)
+
+        return mc
+    }
+    
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertView(title: "Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", delegate: self, cancelButtonTitle: "OK")
+        sendMailErrorAlert.show()
+    }
+    
+    // MARK: MFMailComposeViewControllerDelegate Method
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+
+    
+    
+    
+//
+//    @IBAction func Back(_ sender: Any) {
+//        self.navigationController?.popViewController(animated: true)
+//    }
+//
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        emailText = """
+        Hej,
+        
+        Jag söker härmed ersättning för tåget mellan
+        \(String(describing: fromStation!)) to \(String(describing: String(describing: toStation!)))
+        som skulle ankommit \(String(describing: expectedArrival!)), men inte kom fram förrän \(String(describing: String(describing: actualArrival!))).
+        Jag köpte biljetten via \(String(describing: String(describing: paymentType!))) och vill ha kontant ersättning insatt på min bank enligt nedan:
+        Bank: \(String(describing: bank!))
+        Clearingnummer: \(String(describing: clearingNumber!))
+        Bank: \(String(describing: account!))
+        Personnummer: \(socialSecurity!)
+        
+        Hälsningar
+        \(String(describing: name!))
+        """
+        
+        EmailTextInput.text = emailText
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+   
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Reseersättaren/ViewController.swift
+++ Reseersättaren/ViewController.swift
@@ -8,18 +8,142 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
 
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate {
+    @IBOutlet weak var PaymentPicker: UIPickerView!
+    @IBOutlet weak var NameInput: UITextField!
+    @IBOutlet weak var SocialSecurityInput: UITextField!
+    @IBOutlet weak var FromStationInput: UITextField!
+    @IBOutlet weak var ToStationInput: UITextField!
+    @IBOutlet weak var ExpectedArrivalInput: UIDatePicker!
+    @IBOutlet weak var ActualArrivalInput: UIDatePicker!
+    @IBOutlet weak var BankInput: UITextField!
+    @IBOutlet weak var ClearingInput: UITextField!
+    @IBOutlet weak var BankAccountInput: UITextField!
+    
+    let dateFormatter = DateFormatter()
+    
+    var paymentPickerData: [String] = [String]()
+    var selectedPaymentType: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.hideKeyboardWhenTappedAround()
+        
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
+        
+        paymentPickerData = ["Kort", "App", "Jojo-kort"]
+        selectedPaymentType = paymentPickerData[0]
+        
+        PaymentPicker.delegate = self
+        PaymentPicker.dataSource = self
+        NameInput.delegate = self
+        SocialSecurityInput.delegate = self
+        FromStationInput.delegate = self
+        ToStationInput.delegate = self
+        BankInput.delegate = self
+        ClearingInput.delegate = self
+        BankAccountInput.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func setInputInvalid(input: UIView){
+        input.layer.borderColor = UIColor.red.cgColor
+        input.layer.borderWidth = 1.0
+    }
+    
+    func setInputValid(input: UIView) {
+        input.layer.borderColor = UIColor.black.cgColor
+        input.layer.borderWidth = 0.0
+    }
+    
+    func checkTextFieldValid(input: UITextField) -> Bool {
+        if (input.text == "") {
+            setInputInvalid(input: input)
+            return false
+        }
+        return true
+    }
+    
+    func validateInput() -> Bool {
+        var valid = true
+        
+        valid = checkTextFieldValid(input: NameInput) && valid
+        valid = checkTextFieldValid(input: SocialSecurityInput) && valid
+        valid = checkTextFieldValid(input: FromStationInput) && valid
+        valid = checkTextFieldValid(input: ToStationInput) && valid
+        valid = checkTextFieldValid(input: BankInput) && valid
+        valid = checkTextFieldValid(input: ClearingInput) && valid
+        valid = checkTextFieldValid(input: BankAccountInput) && valid
+        
+        return valid
+    }
 
-
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return paymentPickerData.count;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return paymentPickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedPaymentType = paymentPickerData[row]
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if (textField.text == "") {
+            setInputInvalid(input: textField)
+        }
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        setInputValid(input: textField)
+    }
+    
+    
+ 
+    @IBAction func GoToSendEmail(_ sender: Any) {
+        if (!validateInput()) {
+            return
+        }
+        performSegue(withIdentifier: "SkanetrafikenToSendEmail", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "SkanetrafikenToSendEmail") {
+            if let send = segue.destination as? SendEmailViewController {
+                send.name = NameInput.text!
+                send.socialSecurity = SocialSecurityInput.text!
+                send.fromStation = FromStationInput.text!
+                send.toStation = ToStationInput.text!
+                send.expectedArrival = dateFormatter.string(from: ExpectedArrivalInput.date)
+                send.actualArrival = dateFormatter.string(from: ActualArrivalInput.date)
+                send.bank = BankInput.text!
+                send.clearingNumber = ClearingInput.text!
+                send.account = BankAccountInput.text!
+                send.paymentType = selectedPaymentType!
+            }
+        }
+    }
 }
 
